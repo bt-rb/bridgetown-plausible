f35f05c70db4686487cf06819ef93e17d8fc7ff7
whysthatso <git@whysthatso.net>
2022-09-08 17:56:33 +0300

    feat: ask for server name during automation run, fall back to default

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -1,15 +1,26 @@
 say_status :plausible, "Installing the bridgetown-plausible plugin..."
 
 domain_name = ask("What's your Plausible domain?")
-
+server_name = ask("What's your Plausible server? Leave blank if plausible.io")
 add_bridgetown_plugin "bridgetown-plausible"
 
-append_to_file "bridgetown.config.yml" do
-  <<~YAML
+if server_name == ""
+  append_to_file "bridgetown.config.yml" do
+    <<~YAML
 
-    plausible:
-      domain: #{domain_name}
-  YAML
+      plausible:
+        domain: #{domain_name}
+    YAML
+  end
+else
+  append_to_file "bridgetown.config.yml" do
+    <<~YAML
+
+      plausible:
+        domain: #{domain_name}
+        server: #{server_name}
+    YAML
+  end
 end
 
 say_status :plausible, "All set! Double-check the plausible block in your config file and review docs at"
