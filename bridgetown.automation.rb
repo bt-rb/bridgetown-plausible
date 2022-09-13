b6370188a2898a87fbdc9a0c134a0ef1148d3e28
Andreas Wagner <github.com@whysthatso.net>
2022-09-13 22:21:31 +0300

    feat: make phrasing in automation explicit
    Co-authored-by: Andrew Mason <andrewmcodes@protonmail.com>

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -1,7 +1,7 @@
 say_status :plausible, "Installing the bridgetown-plausible plugin..."
 
 domain_name = ask("What's your Plausible domain?")
-server_name = ask("What's your Plausible server? Leave blank if plausible.io")
+server_name = ask("If you are self-hosting Plausible, what's your instance domain? Leave blank if not self-hosting to default to plausible.io")
 add_bridgetown_plugin "bridgetown-plausible"
 
 if server_name == ""
